fc54dd38ffc70a23788cf3f347064a84f8cb2cca
aadil42 <adilkelawala@gmail.com>
2023-05-24 11:16:36 +0530

    validating form on submit and showing appropriate errors if needed

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
+
+  // const inputRef = useRef();
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const changeHandler = (e) => {
+    setEnteredValue(e.target.value);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if(!enteredValue) {
+      setIsValid(false);
+      return;
+    }
+    setIsValid(true);
+    console.log('success');
+  }
+
+  const formClass = isValid ? 'form-control' : 'form-control invalid';
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={submitHandler}>
+      <div className={formClass}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input onChange={changeHandler} value={enteredValue} type='text' id='name' />
+        {!isValid && <p className="error-text">input field must not be empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
