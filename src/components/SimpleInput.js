a4f3fab4123a7a3a3a180a8936409ccc9e88efd8
aadil42 <adilkelawala@gmail.com>
2023-05-24 13:15:02 +0530

    making the component code leaner

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,42 +1,37 @@
-import {useRef, useState, useEffect} from "react";
+import {useState} from "react";
 
 const SimpleInput = (props) => {
-
-  // const inputRef = useRef();
   const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsValid] = useState(false);
   const [isInputTouched, setIsInputTouched] = useState(false);
+
+  const enteredNameIsValid = enteredValue.trim() !== ''; 
+  const isInvalid = (!enteredNameIsValid && isInputTouched) ? true : false;
+  const formClass = (isInvalid) ? 'form-control invalid' : 'form-control';
 
   const changeHandler = (e) => {
     setEnteredValue(e.target.value);
   }
-
-  // if input is valid then sending it off to the server (simulating it, there's no real server).
-  useEffect(() => {
-    if(isValid) {
-      // seding it to server
-      console.log('sending to server');
-    }
-  },[isValid]);
   
+  const blurHandler = () => {
+    setIsInputTouched(true);
+  }
+
   const submitHandler = (event) => {
     event.preventDefault();
     setIsInputTouched(true);
-    if(!enteredValue) {
-      setIsValid(false);
-      return;
-    }
-    setIsValid(true);
+    if(!enteredNameIsValid) return; // we are not checking isInvalid because react schedules the states changes and we won't have the latest value for invalid.
+    // true we are changing isTouched state above but we won't have the latest invalid at this point
     console.log('success');
+    setEnteredValue('');
+    setIsInputTouched(false);
   }
 
-  const formClass = (!isValid && isInputTouched) ? 'form-control invalid' : 'form-control';
   return (
     <form onSubmit={submitHandler}>
       <div className={formClass}>
         <label htmlFor='name'>Your Name</label>
-        <input onChange={changeHandler} value={enteredValue} type='text' id='name' />
-        {!isValid && isInputTouched && <p className="error-text">input field must not be empty!</p>}
+        <input onBlur={blurHandler} onChange={changeHandler} value={enteredValue} type='text' id='name' />
+        {isInvalid && <p className="error-text">input field must not be empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
