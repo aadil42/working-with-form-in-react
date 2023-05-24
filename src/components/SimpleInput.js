56f21df044c698439ff3c63fa1460fde432adead
aadil42 <adilkelawala@gmail.com>
2023-05-24 11:32:27 +0530

    making the input as invalid  initially so we don't send request in useEffect when the component loads.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,17 +1,27 @@
-import {useRef, useState} from "react";
+import {useRef, useState, useEffect} from "react";
 
 const SimpleInput = (props) => {
 
   // const inputRef = useRef();
   const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(false);
+  const [isInputTouched, setIsInputTouched] = useState(false);
 
   const changeHandler = (e) => {
     setEnteredValue(e.target.value);
   }
 
+  // if input is valid then sending it off to the server (simulating it, there's no real server).
+  useEffect(() => {
+    if(isValid) {
+      // seding it to server
+      console.log('sending to server');
+    }
+  },[isValid]);
+  
   const submitHandler = (event) => {
     event.preventDefault();
+    setIsInputTouched(true);
     if(!enteredValue) {
       setIsValid(false);
       return;
@@ -20,13 +30,13 @@ const SimpleInput = (props) => {
     console.log('success');
   }
 
-  const formClass = isValid ? 'form-control' : 'form-control invalid';
+  const formClass = (!isValid && isInputTouched) ? 'form-control invalid' : 'form-control';
   return (
     <form onSubmit={submitHandler}>
       <div className={formClass}>
         <label htmlFor='name'>Your Name</label>
         <input onChange={changeHandler} value={enteredValue} type='text' id='name' />
-        {!isValid && <p className="error-text">input field must not be empty!</p>}
+        {!isValid && isInputTouched && <p className="error-text">input field must not be empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
