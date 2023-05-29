e71ef67a3cc3ea1791a1e6dc0fb6e518656f774e
aadil42 <adilkelawala@gmail.com>
2023-05-29 09:57:40 +0530

    making submit button disable if the input is invalid

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,10 +3,16 @@ import {useState} from "react";
 const SimpleInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isInputTouched, setIsInputTouched] = useState(false);
-
+  
   const enteredNameIsValid = enteredValue.trim() !== ''; 
   const isInvalid = (!enteredNameIsValid && isInputTouched) ? true : false;
   const formClass = (isInvalid) ? 'form-control invalid' : 'form-control';
+
+  let isFormValid = false;
+
+  if(enteredNameIsValid) {
+    isFormValid = true;
+  }
 
   const changeHandler = (e) => {
     setEnteredValue(e.target.value);
@@ -19,6 +25,8 @@ const SimpleInput = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
     setIsInputTouched(true);
+
+    // now we don't even need to check this condition, because we're not allowing user to even submit if the field is empty. so this function will never run on an empty field
     if(!enteredNameIsValid) return; // we are not checking isInvalid because react schedules the states changes and we won't have the latest value for invalid.
     // true we are changing isTouched state above but we won't have the latest invalid at this point
     console.log('success');
@@ -34,7 +42,7 @@ const SimpleInput = (props) => {
         {isInvalid && <p className="error-text">input field must not be empty!</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!isFormValid}>Submit</button>
       </div>
     </form>
   );
