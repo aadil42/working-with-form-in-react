a1f4b75d517dcf86a6d7c21ad0ef3116bc520f7f
aadil42 <adilkelawala@gmail.com>
2023-05-29 11:21:10 +0530

    added validation for email field just like name-text field

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,28 +1,37 @@
-import {useState} from "react";
+import {useState, useCallback} from "react";
 
 const SimpleInput = (props) => {
+
+  const [isButtonTouched, setIsButtonTouched] = useState(false);
 
   // for name/text
   const [enteredValue, setEnteredValue] = useState('');
   const [isInputTouched, setIsInputTouched] = useState(false);
-  const [isButtonTouched, setIsButtonTouched] = useState(false);
 
   const enteredNameIsValid = enteredValue.trim() !== ''; 
   const isInvalid = (!enteredNameIsValid && isInputTouched) ? true : false;
+  const inputClass = (isInvalid) ? 'form-control invalid' : 'form-control';
 
   // for email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const validateEmail = useCallback((email) => {
+    return emailRegex.test(email);
+  }, [emailRegex]);
+
   const [enteredEmail, setEnteredEmail] = useState('');
+  const [isEmailTouched, setIsEmailTouched] = useState(false);
+
+  const enteredEmailIsValid = validateEmail(enteredEmail);
+  const isEmailInvalid = (!enteredEmailIsValid && isEmailTouched) ? true : false;
+  const emailClass = (isEmailInvalid) ? 'form-control invalid' : 'form-control';
 
 
-  // cheking overall form.
-  const formClass = (isInvalid) ? 'form-control invalid' : 'form-control';
-
+  // for overall form.
   let isFormValid = false;
-
-  if(enteredNameIsValid) {
+  if(enteredNameIsValid && enteredEmailIsValid) {
     isFormValid = true;
   }
-
 
   // state handlers.
   const changeHandler = (e) => {
@@ -35,26 +44,34 @@ const SimpleInput = (props) => {
     }
   }
   
-  const blurHandler = () => {
-    setIsInputTouched(true);
+  const blurHandler = (e) => {
+    if(e.target.id === 'name') {
+      setIsInputTouched(true);
+    }
+    if(e.target.id === 'email') {
+      setIsEmailTouched(true);
+    }
     setIsButtonTouched(true);
   }
 
   const submitHandler = (event) => {
     event.preventDefault();
     setIsInputTouched(true);
+    setIsEmailTouched(true);
     setIsButtonTouched(true);
-    // now we don't even need to check this condition, because we're not allowing user to even submit if the field is empty. so this function will never run on an empty field
-    if(!enteredNameIsValid) return; // we are not checking isInvalid because react schedules the states changes and we won't have the latest value for invalid.
+    if(!enteredNameIsValid || !enteredEmailIsValid) return; // we are not checking isInvalid because react schedules the states changes and we won't have the latest value for invalid.
     // true we are changing isTouched state above but we won't have the latest invalid at this point
     console.log('success');
+    setEnteredEmail('');
+    setIsEmailTouched(false);
     setEnteredValue('');
     setIsInputTouched(false);
+    setIsButtonTouched(false);
   }
 
   return (
     <form onSubmit={submitHandler}>
-      <div className={formClass}>
+      <div className={inputClass}>
         <label htmlFor='name'>Your Name</label>
         <input 
         onBlur={blurHandler} 
@@ -64,15 +81,15 @@ const SimpleInput = (props) => {
         id='name' />
         {isInvalid && <p className="error-text">input field must not be empty!</p>}
       </div>
-      <div className={formClass}>
+      <div className={emailClass}>
         <label htmlFor='email'>Your Email</label>
         <input 
-        // onBlur={blurHandler} 
+        onBlur={blurHandler} 
         onChange={changeHandler} 
         value={enteredEmail} 
         type='email' 
         id='email' />
-        {/* {isInvalid && <p className="error-text">Enter a valid Email.</p>} */}
+        {isEmailInvalid && <p className="error-text">Enter a valid Email.</p>}
       </div>
       <div className="form-actions">
         <button disabled={!isFormValid && isButtonTouched}>Submit</button>
