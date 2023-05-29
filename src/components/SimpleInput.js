5b0edba66801b518324b6cc56cccb3652c42acf4
aadil42 <adilkelawala@gmail.com>
2023-05-29 09:59:29 +0530

    ok, so initially we're enabling the submit button on the form and after the first try we'll disable it on invalid field

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,7 +3,8 @@ import {useState} from "react";
 const SimpleInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isInputTouched, setIsInputTouched] = useState(false);
-  
+  const [isButtonTouched, setIsButtonTouched] = useState(false);
+
   const enteredNameIsValid = enteredValue.trim() !== ''; 
   const isInvalid = (!enteredNameIsValid && isInputTouched) ? true : false;
   const formClass = (isInvalid) ? 'form-control invalid' : 'form-control';
@@ -25,7 +26,7 @@ const SimpleInput = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
     setIsInputTouched(true);
-
+    setIsButtonTouched(true);
     // now we don't even need to check this condition, because we're not allowing user to even submit if the field is empty. so this function will never run on an empty field
     if(!enteredNameIsValid) return; // we are not checking isInvalid because react schedules the states changes and we won't have the latest value for invalid.
     // true we are changing isTouched state above but we won't have the latest invalid at this point
@@ -42,7 +43,7 @@ const SimpleInput = (props) => {
         {isInvalid && <p className="error-text">input field must not be empty!</p>}
       </div>
       <div className="form-actions">
-        <button disabled={!isFormValid}>Submit</button>
+        <button disabled={!isFormValid && isButtonTouched}>Submit</button>
       </div>
     </form>
   );
